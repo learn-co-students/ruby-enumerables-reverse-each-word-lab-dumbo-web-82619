2b9a95577e4728f48c4cd2120e711295b2a4fc36
glorious-genre-8033 <namontesino@gmail.com>
2019-08-16 23:44:40 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_each_word(string)
+  sentence_array = string.split(' ')
+  reversed_array = sentence_array.each{|word| word.reverse!}
+  reversed_array.join(' ')
+end
+
+
+
+def reverse_each_word(string)
+  sentence_array = string.split(' ')
+  reversed_array = sentence_array.collect{|word| word.reverse!}
+  reversed_array.join(' ')
+end
+
