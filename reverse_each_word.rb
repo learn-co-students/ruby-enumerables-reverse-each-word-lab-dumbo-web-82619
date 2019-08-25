a3e4fd43f4f5037bdcbecc078a64375b20f5a65b
James Gibbel <jamesgibbel@gmail.com>
2019-08-25 23:27:12 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,20 @@
-def reverse_each_word(string)
+def reverse_each_word_no(string)
   new = string.split(/ /)
   ugh = []
-  for x in 0..new.length-1 do 
+  for x in 0..new.length-1 do
+    puts new[x]
     ugh.push(new[x].reverse)
   end
   p ugh
   p new
   ugh = ugh.join(' ')
   return ugh
+end
+
+    
+def reverse_each_word(string)
+    colarr = string.split(" ").collect{ |word| word.reverse}
+    colarr.join(" ")
 end
 
 reverse_each_word('hi hello how are you')
